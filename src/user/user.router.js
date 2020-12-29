2d970b59371d80ebe452377f4c49a7ba380f762c
logansbacca <61002844+logansbacca@users.noreply.github.com>
2020-12-29 18:20:16 +0100

    Update user.router.js

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -10,7 +10,7 @@ router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.delete);
 router.post('/login',userController.loginUser);
-router.delete('/logout', userController.logoutUser)
+router.delete('/logout/:id/:token', userController.logoutUser)
 
 router.post("/create",(req,res) =>{
     res.send("user created")
