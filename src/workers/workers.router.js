2ae5407d3ec3c38479be2a05eb748ef395f87028
logan <logans.bacca@gmail.com>
2021-01-05 00:28:34 +0100

    filter

--- src/workers/workers.router.js
+++ src/workers/workers.router.js
@@ -8,7 +8,7 @@ router.get('/:id', workersController.getWorker);
 router.post('/', workersController.createWorker);
 router.put('/:id', workersController.updateWorker);
 router.delete('/:id', workersController.deleteWorker);
-router.get('/filter', workersController.filterWorkers);
+router.post('/filter', workersController.filterWorkers);
 
 module.exports = router;
 
